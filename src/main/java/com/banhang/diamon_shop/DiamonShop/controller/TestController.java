23682c48d38b41e471da4d0916656874e72c9d5b
duylinhaaa125 <duylinh19981007@gmail.com>
2023-08-19 16:29:08 +0800

    test dev branch

---
+++ src/main/java/com/banhang/diamon_shop/DiamonShop/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.banhang.diamon_shop.DiamonShop.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.banhang.diamon_shop.DiamonShop.dao.HomeDao;
+import com.banhang.diamon_shop.DiamonShop.service.HomeServiceImpl;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	HomeServiceImpl homeService;
+	
+	@RequestMapping(value={"/", "/trang-chu"})
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		ModelAndView mv = new ModelAndView("user/index");
+		mv.addObject("slides" , homeService.GetDataSlide());
+		return mv;
+	}
+	
+	@RequestMapping(value= "/product")
+	public ModelAndView Product(HttpServletResponse response) throws IOException{
+		return new ModelAndView("user/product");
+	}
+}
